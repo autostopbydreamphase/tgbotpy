0caa4b7ee1537931aa381fa170d1bdc833430bcd
muffinnotfoundiwnl <119512079+muffinnotfoundiwnl@users.noreply.github.com>
2022-12-14 16:35:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-token = '5880477181:AAGMvyRSiYoGynUNrre6LGxdllmU9ox8VL4';
+token = '5880477181:AAGMvyRSiYoGynUNrre6LGxdllmU9ox8VL4'
+URL1 = 'https://game-tournaments.com/csgo/matches'
+URL = 'https://www.hltv.org/matches'
